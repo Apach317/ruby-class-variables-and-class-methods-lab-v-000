08254cf1be28d5608afa0fc19a0256164ded7080
Ron Lynch <lynch.ronald1@gmail.com>
2018-09-10 00:01:31 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+class song
+  attr_accessor :name, :artist, :genres
+  @@count = 0
+  @@genres = []
+  def initialize
+    @@count += 1
+    @@genres << genre
+  end
+
+  def count
+    @@count
+  end
+
+  def genres
+    @@genres.uniq
+  end
+end
