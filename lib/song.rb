1fad7802b2c0bb0917d761ea2f6078ebb6c26e63
Ron Lynch <lynch.ronald1@gmail.com>
2018-09-10 00:14:13 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,14 +28,14 @@ class Song
     @@artists.uniq
   end
 
-  def genre_count
+  def self.genre_count
     @@genres.each do |type|
       @@genre_count[type] = @@genres.count(genre)
     end
     @@genre_count
   end
 
-  def artists_count
+  def self.artists_count
     @@artists.each do |art|
       @@artists_count[art] = @@artists.count(artist)
     end
